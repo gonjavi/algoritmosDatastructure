1a3660441ad5e4bd332725f8a754ee60813e34c2
manci <gmancillapaz@gmail.com>
2021-07-25 11:04:30 -0500

    -

--- reversestring/index.js
+++ reversestring/index.js
@@ -11,13 +11,26 @@
 }
  */
 
-function reverse(str) {
-  return arr
+/* function reverse(str) {
+  return str
     .split('')
     .reverse()
     .join('');
-}
+} */
 
 // solution 2
+/* Crear un string vacio llamado reversed
+    crear un foreach caracter en el string
+        tome el caracter y añadalo al comienzo de reversed
+    
+    retornar el valor reversado
+  */
+function reverse(str) {
+  let reversed = '';
 
+  for (const character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+} 
 module.exports = reverse;
