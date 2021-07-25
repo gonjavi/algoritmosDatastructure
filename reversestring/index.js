b5fb7d69f960aa4249465355771a100a2f3adcdd
manci <gmancillapaz@gmail.com>
2021-07-25 10:32:35 -0500

    -

--- reversestring/index.js
+++ reversestring/index.js
@@ -1,8 +1,23 @@
 
+// solution 1
+/* function reverse(str) {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+} */
+
+/* function reverse(str) {
+  return arr.split('').reverse().join('');
+}
+ */
 
 function reverse(str) {
-
-  
+  return arr
+    .split('')
+    .reverse()
+    .join('');
 }
+
+// solution 2
 
 module.exports = reverse;
