f6fac11f7e1e06257ed12e7f38771f5432cb0a2a
manci <gmancillapaz@gmail.com>
2021-07-26 22:21:06 -0500

    -

---
+++ maxchar/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  // for - of to iterate through an array or a string
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+ 
+  // for - in to iterate through an object
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+module.exports = maxChar;
--- palindrome/index.js
+++ palindrome/index.js
@@ -7,12 +7,6 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-// solution 1
-/* function palindrome(str) {
-  const reversed = str.split('').reverse().join('');
-  return str === reversed;
-} */
-
 // solution 2
 /* function palindrome(str) {
   const reversed = str
@@ -24,10 +18,15 @@
 } */
 
 // solution 3
-function palindrome(str) {
+/* function palindrome(str) {
   return str.split('').every((char, i) => {
     return char === str[str.length - i - 1];
   });
+} */
+// solution 1
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
 }
 module.exports = palindrome;
 
