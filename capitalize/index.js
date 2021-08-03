f13de288ca8493ed81f997c9138f34ee9f6ac058
manci <gmancillapaz@gmail.com>
2021-08-02 21:18:37 -0500

    components part 2

--- capitalize/index.js
+++ capitalize/index.js
@@ -10,9 +10,21 @@
 function capitalize(str) {
   const words = [];
   for (let char of str.split(' ')) {   
-    words.push(char[0].toUpperCase() + char.slice(1))
+    words.push( char[0].toUpperCase() + char.slice(1))
   } 
   return words.join(' ');
 }
 
 module.exports = capitalize;
+
+/* function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let index = 1; index < str.length; index++) {
+    if ( str[index - 1] === ' ') {
+      result += str[index].toUpperCase();
+    } else {
+      result += str[index];
+    }
+  }
+  return result;
+} */
