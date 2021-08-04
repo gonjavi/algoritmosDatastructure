e846da73e2fe546e180ec5c03ff8bee3b7126c8d
manci <gmancillapaz@gmail.com>
2021-08-03 21:47:27 -0500

    components part 2

--- steps/index.js
+++ steps/index.js
@@ -16,8 +16,8 @@
 //       '##  '
 //       '### '
 //       '####'
-
-function steps(n) {
+//solution 1
+/* function steps(n) {
   for (let row = 0; row < n; row++) {
     let stair = '';
     for (let column = 0; column < n; column++) {
@@ -29,6 +29,38 @@ function steps(n) {
     }
     console.log(stair);
   } 
+} */
+// solution 2
+/* function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+} */
+// solution 3
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
 }
 
 module.exports = steps;
