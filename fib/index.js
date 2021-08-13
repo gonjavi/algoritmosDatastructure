af0354182212dca720376fffacff85d65dab87fd
manci <gmancillapaz@gmail.com>
2021-08-12 22:08:35 -0500

    components part 2

--- fib/index.js
+++ fib/index.js
@@ -8,10 +8,52 @@
 // Example:
 //   fib(4) === 3
 
+// solution 1
+/* function fib(n) {
+  const result = [0, 1];
 
-function fib(n) {
-  
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1 ];
+    const b = result[i - 2 ]
+
+    result.push(a + b);
+  }
+  return result[n];
+} */
+
+/* function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+} */
+
+// with momoization function - faster
+
+function memoize(fn) {
+  const cache = {}; 
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
 }
+
+function slowfib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowfib);
 
 module.exports = fib;
 
