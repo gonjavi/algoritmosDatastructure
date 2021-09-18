aabb5f641da95d25dbf5d75d99eafb38e5dc463d
manci <gmancillapaz@gmail.com>
2021-09-17 22:55:11 -0500

    -

---
+++ sorting/index.js
@@ -0,0 +1,46 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+// to fins the largest element into the array and push it to the end
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++ ) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j+1]) {
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  // return sorted array
+  return arr;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++ ) {
+    let indexOfMin = i;
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesser =  arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
+}
+
+function mergeSort(arr) {
+
+}
+
+function merge(left, right) {
+
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
